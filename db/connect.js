846c736628bd40f44e7fa09b08fe77223843aeca
ashish <ashish.badgujar@bitsmith.tech>
2022-07-16 16:54:52 +0530

    mongo uri

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export default async () => {
     try {
 
-        const mongoURI = process.env.MONGO_URI;
+        const mongoURI = "mongodb+srv://Ashish:ashish@foodweb.cwoie.mongodb.net/?retryWrites=true&w=majority"
         const db = "kanban"
         const conn = await mongoose.connect(`${mongoURI}/${db}`)
         console.log(`MongoDB connect ${conn.connection.host}`)
