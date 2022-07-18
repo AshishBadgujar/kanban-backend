d0ba1f392d012ff9bb1b3d44e705d9a378279a36
ashish <ashish.badgujar@bitsmith.tech>
2022-07-18 11:19:41 +0530

    added headers

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ db();
 
 const app = express()
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(express.json())
 
 app.get('/api/kanban/board', async (req, res) => {
